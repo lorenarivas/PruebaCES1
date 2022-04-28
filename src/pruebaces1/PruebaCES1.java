ea3c2966bc55b9bb2ba958c14fb84666db008694
lorena.rl1995@gmail.com <>
2022-04-28 18:43:00 -0500

    PruebaCES1

---
+++ src/pruebaces1/PruebaCES1.java
@@ -0,0 +1,17 @@
+
+package pruebaces1;
+
+/**
+ *
+ * @author Irann
+ */
+public class PruebaCES1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GIT HUB Rivas 18042022");
+    }
+    
+}
